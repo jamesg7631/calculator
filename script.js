e98e78e497238d7185a97de6dd966bc7f3ceaaa5
James Glover <james.glover7@outlook.com>
2026-01-12 18:53:28 +0000

    Added pseudocode need to debug and get working

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 function main() {
+  const operands = ["+", "-", "*", "/"];
   console.log("Starting Program");
+  let calculatorOutput =
+    document.querySelector(".calculator-output").textContent;
+  const btnContainer = document.querySelector(".calculator-buttons");
+  const equalButton = document.querySelector(".equal");
+  // Probably a good idea to separate into a named function
+  //
+  // Adds User Input
+  btnContainer.addEventListener("click", (e) => {
+    let userInput = e.target.value;
+    if (!Number.isNaN()) {
+      if (userInput.length > 0 && calculatorOutput.at(-1) in operands) {
+        calculatorOutput += " ";
+      }
+      calculatorOutput += userInput.toString();
+    } else {
+      if (validateOperand(calculatorOutput, userInput, operands)) {
+        calculatorOutput += " " + userInput.toString();
+      }
+    }
+  });
+
+  // Calculate Result
+  equalButton.addEventListener("click", (e) =>
+    calculateResult(calculatorOutput),
+  );
+}
+
+function calculateResult(calculatorScreenOutput, operands) {
+  const operand = calculatorScreenOutput.find((char) =>
+    operands.includes(char),
+  );
+  if (!(operand === undefined)) {
+    const parts = calculatorScreenOutput.split(operand);
+    const result = operator(
+      parts[1],
+      Number.parseInt(operand(0)),
+      Number.parseInt(operand(1)),
+    );
+    console.log(`Result: ${result}`);
+  }
+}
+
+function validateOperand(currentScreen, userInput, operand) {
+  if (!operand.includes(userInput)) {
+    return false;
+  }
+  if (currentScreen.length === 0) {
+    return false;
+  }
+
+  // Ensure there cannot exist duplicate operands
+  if (currentScreen.some((char) => operand.includes(char))) {
+    return false;
+  }
+  return true;
 }
 
 function add(number1, number2) {
